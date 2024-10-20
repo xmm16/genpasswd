5bf8bd46b13921ace8255a65dfa3a94f33983707
xmm <typescriptlover@tuta.io>
2024-10-20 10:44:33 -0300

    fix: Move to LLVM formatting guidelines, and add build/ to .gitignore

--- src/main.c
+++ src/main.c
@@ -7,20 +7,18 @@
 
 #define BUFFER_SIZE 512
 
-const char *
-__scudo_default_options(void)
-{
-	return "dealloc_type_mismatch=true:zero_contents=true:pattern_fill_"
-	       "contents=true:delete_size_mismatch=true:release_to_os_"
-	       "interval_ms=-"
-	       "1";
+const char *__scudo_default_options(void) {
+  return "dealloc_type_mismatch=true:zero_contents=true:pattern_fill_"
+         "contents=true:delete_size_mismatch=true:release_to_os_"
+         "interval_ms=-"
+         "1";
 }
 
 /*
  * Linked list of all calls to malloc.
  */
 struct allocation_node {
-	struct allocation_node *next;
+  struct allocation_node *next;
 };
 
 static struct allocation_node *alloc_head = NULL;
@@ -28,110 +26,97 @@ static struct allocation_node *alloc_head = NULL;
 /*
  * Free all mallocs within the linked list.
  */
-static void
-free_allocations(void)
-{
-	struct allocation_node *tmp;
-	struct allocation_node *n;
+static void free_allocations(void) {
+  struct allocation_node *tmp;
+  struct allocation_node *n;
 
-	n = alloc_head;
-	alloc_head = NULL;
+  n = alloc_head;
+  alloc_head = NULL;
 
-	while (n != NULL) {
-		tmp = n->next;
-		free(n);
-		n = tmp;
-	}
+  while (n != NULL) {
+    tmp = n->next;
+    free(n);
+    n = tmp;
+  }
 }
 
 /*
  * Track all mallocs so it will be freed later.
  */
-static void *
-malloc_wrapper(size_t size)
-{
-	struct allocation_node *node;
-	void *p;
+static void *malloc_wrapper(size_t size) {
+  struct allocation_node *node;
+  void *p;
 
-	node = malloc(sizeof *node + size);
-	if (node == NULL)
-		abort();
+  node = malloc(sizeof *node + size);
+  if (node == NULL)
+    abort();
 
-	node->next = alloc_head;
-	alloc_head = node;
+  node->next = alloc_head;
+  alloc_head = node;
 
-	p = node + 1;
-	return p;
+  p = node + 1;
+  return p;
 }
 
 // Print HEX
-static char *
-print_hex(const void *buf, const size_t len)
-{
-	const unsigned char *b;
-	char *p;
+static char *print_hex(const void *buf, const size_t len) {
+  const unsigned char *b;
+  char *p;
 
-	b = buf;
-	p = malloc_wrapper(len * 2 + 4);
+  b = buf;
+  p = malloc_wrapper(len * 2 + 4);
 
-	/* the library supplies a few utility functions like the one below */
-	return sodium_bin2hex(p, len * 2 + 1, b, len);
+  /* the library supplies a few utility functions like the one below */
+  return sodium_bin2hex(p, len * 2 + 1, b, len);
 }
 
-static char *
-print_b64(const void *buf, const size_t len)
-{
-	const unsigned char *b;
-	char *p;
-	b = buf;
-	p = malloc_wrapper(len * 4 + 8);
-	return sodium_bin2base64(p, len * 2 + 1, b, len,
-		sodium_base64_VARIANT_ORIGINAL);
+static char *print_b64(const void *buf, const size_t len) {
+  const unsigned char *b;
+  char *p;
+  b = buf;
+  p = malloc_wrapper(len * 4 + 8);
+  return sodium_bin2base64(p, len * 2 + 1, b, len,
+                           sodium_base64_VARIANT_ORIGINAL);
 }
 
 /*
  * Dirty STACK to detect zeroing errors
  */
-static void
-dirty(void)
-{
-	unsigned char memory[512];
-	memset(memory, 'c', sizeof memory);
-	void sodium_stackzero(const size_t memory);
+static void dirty(void) {
+  unsigned char memory[512];
+  memset(memory, 'c', sizeof memory);
+  void sodium_stackzero(const size_t memory);
 }
 
-int
-main(void)
-{
-	dirty();
-	atexit(&free_allocations);
-	unsigned char k[crypto_generichash_KEYBYTES_MAX]; // Key
-	unsigned char h[crypto_generichash_BYTES_MAX];	  // Hash output
-	unsigned char m[BUFFER_SIZE];			  // Message
-	size_t mlen = 0;
+int main(void) {
+  dirty();
+  atexit(&free_allocations);
+  unsigned char k[crypto_generichash_KEYBYTES_MAX]; // Key
+  unsigned char h[crypto_generichash_BYTES_MAX];    // Hash output
+  unsigned char m[BUFFER_SIZE];                     // Message
+  size_t mlen = 0;
 
-	if (sodium_init() < 0) {
-		printf("\33[0:31m\\]FATAL ERROR: could NOT initialize "
-		       "cryptographic "
-		       "engine, aborting.\33[0m\\]\n"); // IT IS NOT SAFE TO
-							// RUN
-		return 1;
-	}
-	printf("\033[22;34mCryptographic engine started "
-	       "successfully!\033[0m\n");
+  if (sodium_init() < 0) {
+    printf("\33[0:31m\\]FATAL ERROR: could NOT initialize "
+           "cryptographic "
+           "engine, aborting.\33[0m\\]\n"); // IT IS NOT SAFE TO
+                                            // RUN
+    return 1;
+  }
+  printf("\033[22;34mCryptographic engine started "
+         "successfully!\033[0m\n");
 
-	sodium_memzero(k, sizeof k);
-	sodium_memzero(h, sizeof h);
-	sodium_memzero(m, sizeof m);
+  sodium_memzero(k, sizeof k);
+  sodium_memzero(h, sizeof h);
+  sodium_memzero(m, sizeof m);
 
-	randombytes_buf(k, sizeof k);
-	randombytes_buf(h, sizeof h);
-	randombytes_buf(m, sizeof m);
+  randombytes_buf(k, sizeof k);
+  randombytes_buf(h, sizeof h);
+  randombytes_buf(m, sizeof m);
 
-	crypto_generichash(h, sizeof h, m, mlen, k, sizeof k);
-	printf("Result pre-scramble (HEXADECIMAL): %s\n",
-		print_hex(h, sizeof h));
-	printf("Result scrambled for password usage (Base64): %s\n",
-		print_b64(h, sizeof h));
-	return 0;
+  crypto_generichash(h, sizeof h, m, mlen, k, sizeof k);
+  printf("Result pre-scramble (HEXADECIMAL): %s\n", print_hex(h, sizeof h));
+  printf("Result scrambled for password usage (Base64): %s\n",
+         print_b64(h, sizeof h));
+  return 0;
 }
